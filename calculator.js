6479da69ecea5f084e2ca2b805f57c6fd15daae9
ayasalem123 <salem.aya184@gmail.com>
2023-01-19 23:52:42 +0100

    request changes

--- calculator.js
+++ calculator.js
@@ -1,6 +1,9 @@
+const bodyParser = require("body-parser");
 const express=require("express")
 const app=express();
 app.set('view engine','ejs');
 const routes=require("./routes")
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(routes)
+
 app.listen(3002)
--- routes.js
+++ routes.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const { append } = require('vary');
 const router = express.Router();
 router.get("/",(req,res)=>{
     res.render("main.ejs")
 })
+router.use("/result",(req,res)=>{
+    console.log("hi")
+    console.log(req.body)
+    // res.send({result:eval(req.body.firstvalue+req.body.operator+req.body.secondvalue)})
+    res.render("result.ejs",{req:req})
+})
+
 module.exports = router;
